42c41147bbe1a0641016645f144eb00c7c741aee
sansaboon <sansaboon17@gmail.com>
2024-11-11 15:21:44 +0700

    pull

--- src/api/search.js
+++ src/api/search.js
@@ -3,8 +3,12 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 const headerToken = import.meta.env.VITE_API_HEADER
 
 export const getAllPost = (body) => {
-  return axios.post(`${baseUrl}/search`, body, headerToken);
-};
+  return axios.post(`${baseUrl}/search`, body,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+  }
+})};
 
 export const getAllPostByValue = (body) => {
   return axios.post(`${baseUrl}/search/val`, body, headerToken);
